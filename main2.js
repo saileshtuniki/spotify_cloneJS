0a4c6efa02c1a8830568a8bdd90c1d4c417daea5
saileshtuniki <sailesh.tuniki@gmail.com>
2022-06-06 20:19:11 +0530

    third commit

--- main2.js
+++ main2.js
@@ -103,7 +103,7 @@ searchbtn.addEventListener('click', (e) => {
                         albumoutput += 
                         `
                         <div class=" card mb-3 bg-dark text-white " style= "width: 15rem " >
-                            <a style="text-decoration: none" href ="${newresponse.tracks.items[index].href}">
+                            <a style="text-decoration: none" href ="${newresponse.tracks.items[index].preview_url}">
 
                             <div clas="card-body text-white">
                             <img src="${newresponse.images[0].url}">
